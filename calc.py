15651d3001abf89b46f311c657340ce66f6d9007
richie <richie.morrisroe@gmail.com>
2018-07-26 19:00:03 +0100

    got calculator closer to working
    stupid name conflicts, stoopid me.

--- calc.py
+++ calc.py
@@ -1,41 +1,65 @@
 import string as s
-def make_comparator(list):
+
+
+def make_comparator(l):
+    print(l)
+
     def comp(char):
-        if char in list:
+        print(char)
+        if char in l:
             return char
         else:
             pass
-        return(comp)
+        return comp
+
 
 def is_add_op(char):
-    if char in '+-':
+    if char in "+-":
         return char
     else:
         pass
+
+
 is_number = make_comparator(s.digits)
-##start with simple calculator language
-##implement plus, minus and parentheses
+
+# start with simple calculator language
+# implement plus, minus and parentheses
+
+
 def plus(x, y):
     x + y
+
+
 def minus(x, y):
     x - y
+
+
 def multiply(x, y):
-    x*y
+    x * y
+
+
 def divide(x, y):
-    x/y
-while(True):
+    x / y
+
+
+operators = "+-"
+
+while True:
     expr = input("What mathematical operation would you like me to perform?")
     nums = []
     ops = []
+    fullexp = []
     expr.split()
     print(expr)
-    for each in expr:
-        print(type(each))
-        if is_number(each):
-            nums.append(each)
-        # if is_add_op(each):
-        #     ops.append(each)
-            #leave this out first
+    for element in expr:
+        fullexp.append(element)
+        if element in s.digits:
+            nums.append(element)
+        if element in operators:
+            ops.append(element)
+        else:
+            pass
+        # leave this out first
         # if is_bracket(each):
         # for num, op in nums, ops:
         #     res = 0
